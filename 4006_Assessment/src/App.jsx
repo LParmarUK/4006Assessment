3de0a91a0b346c72de4e90a70c74ece179539689
LParmarUK <LeonparmarUK@hotmail.com>
2025-04-29 11:04:30 +0100

    added layout

--- 4006_Assessment/src/App.jsx
+++ 4006_Assessment/src/App.jsx
@@ -4,15 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Conditions from './pages/Conditions';
 import BmiCalculator from './pages/BmiCalculator';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/conditions" element={<Conditions />} />
-        <Route path="/bmi" element={<BmiCalculator />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/conditions" element={<Conditions />} />
+          <Route path="/bmi" element={<BmiCalculator />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
