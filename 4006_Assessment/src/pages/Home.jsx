731ae0caa009055c4bac0f36606499f643e70b53
LParmarUK <LeonparmarUK@hotmail.com>
2025-04-29 10:23:21 +0100

    created to a home page

---
+++ 4006_Assessment/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to St Mary's Health Hub</h1>
+      <p>For personal wellness, fitness tips, and mental health support.</p>
+    </div>
+  );
+};
+
+export default Home;
