bfca7ed84dcae5848f573e786aabc0efeed25a8e
LParmarUK <LeonparmarUK@hotmail.com>
2025-05-08 16:04:12 +0100

    created an easy way to keep colour codes where I need them

---
+++ 4006assessment-new/src/theme/colours.jsx
@@ -0,0 +1,20 @@
+const colours = {
+    // Branding Colours
+    darkBlue: '#002E5D',
+    twickenhamBlue: '#00AEEF',
+    pageBlue: '#3580BB',
+    white: '#FFFFFF',
+  
+    // Dark Mode
+    darkModeBackground: '#121212',
+    lightText: '#f8f9fa',
+  
+    // Navbar Hover
+    navHoverBlue: '#008ec9',
+  
+    // Button Hover
+    darkBlueHover: '#001a3d'
+  };
+  
+  export default colours;
+  
